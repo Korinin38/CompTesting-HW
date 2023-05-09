0028ae024086731a5f1597d418db9e84c4924da9
Korinin38 <artkash@bk.ru>
2023-05-09 09:44:39 +0300

    feat: generate list of testing units
    Refactor function names to satisfy convention

--- lab5/testing.py
+++ lab5/testing.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 from requests import get
+import numpy
 
 
 PERSON_SINGLE_KEYS = ['name', 'height', 'mass', 'hair_color', 'skin_color',
                       'eye_color', 'birth_year', 'gender', 'homeworld', 'created', 'edited', 'url']
 PERSON_LIST_KEYS = ['films', 'species', 'vehicles', 'starships']
 
+# function naming convention: test_*scope_name*[_(single/traverse/random_batch)[_by_data]]
 
-def test_single_person_by_data(person_json):
+
+def test_people_single_by_data(person_json):
     for key in PERSON_SINGLE_KEYS:
         assert key in person_json
         assert type(person_json[key]) is str, f'{person_json[key]} is not str!'
@@ -19,12 +22,12 @@ def test_single_person_by_data(person_json):
     assert len(person_json) == len(PERSON_LIST_KEYS) + len(PERSON_SINGLE_KEYS)
 
 
-def test_traverse_people():
+def test_people_traverse():
     url = "https://swapi.dev/api/people"
     old_count = None
     true_count = 0
     while url != None:
-        print(url, flush=True)
+        print(url)
         response = get(url).json()
         assert response["count"] is not None
 
@@ -36,7 +39,7 @@ def test_traverse_people():
         old_count = response["count"]
         true_count += len(response["results"])
         for person in response["results"]:
-            test_single_person_by_data(person)
+            test_people_single_by_data(person)
             print(".", end="", flush=True)
             assert get(person["url"]).json() == person
         url = response["next"]
@@ -54,7 +57,25 @@ def test_durability(count=1000):
     print()
 
 
+def test_starships_single_by_data(starship_json):
+    pass
+
+
+def test_starships_random_batch(count=10):
+    url = "https://swapi.dev/api/starships"
+    print(url)
+
+    response = get(url).json()
+    assert response["count"] is not None
+
+    testing_set = set(numpy.random.choice(response["count"], count))
+    for unit in testing_set:
+        response = get(url + f"/{unit}/").json()
+        test_starships_single_by_data(response)
+
+
 if __name__ == "__main__":
-    test_traverse_people()
+    test_people_traverse()
     # test_durability()
     test_durability(0)
+    # test_starships_random_batch()
